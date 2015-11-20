34fb2d01b65f01168319e54def937a2fc1fd073c
dylanmodesitt <dylanmodesitt@gmail.com>
2015-11-19 20:55:23 -0800

    Added support for jpg and gif images.

--- assets/php/encode.php
+++ assets/php/encode.php
@@ -1,21 +1,49 @@
 <?php
 // Upload image to ../uploads/(file) so it can be encoded with a message
 $target_dir="../uploads/";
-$target_img = $target_dir . "image.png";
+$target_img = $target_dir.$_FILES["imageUploaded"]["name"] ;
 $img_result = move_uploaded_file($_FILES["imageUploaded"]["tmp_name"], $target_img);
 $givenMessage = $_POST["secretMessage"];
 
 if ($img_result == FALSE) {  } else { }
 
-function encodeImageWithMessage($message, $image) {
-	$imageToModify = imagecreatefrompng($image);
-	list($width, $height) = getimagesize($image);
-	$binaryGiven = messageToBinary($message);
+    function forceImageAsPNG($img_path) {
+        $extension = pathinfo($img_path, PATHINFO_EXTENSION);
+        $image;
+        switch($extension) {
+            case 'jpg':
+            case 'jpeg':
+            case 'JPG':
+            case 'JPEG':
+                $image = imagecreatefromjpeg($img_path);
+                imagepng($image, $img_path);
+                break;
+            case 'gif':
+            case 'GIF':
+                $image = imagecreatefromgif($img_path);
+                imagepng($image, $img_path);
+                break;
+            case 'png':
+            case 'PNG':
+                $image = imagecreatefrompng($img_path);
+                imagepng($image, $img_path);
+                break;
+            default :
+                break;
+        }
+    }
 
-	$x = 0;
-	$y = 0;
+    forceImageAsPNG($target_img);
+
+    function encodeImageWithMessage($message, $image) {
+       $imageToModify = imagecreatefrompng($image);
+       list($width, $height) = getimagesize($image);
+       $binaryGiven = messageToBinary($message);
+
+       $x = 0;
+       $y = 0;
 	//encode the given binary into the image
-	for($i=0;$i<strlen($binaryGiven);$i++) {
+       for($i=0;$i<strlen($binaryGiven);$i++) {
 
         if($i == 0) {
         	$x = 0;
@@ -58,37 +86,37 @@ function encodeImageWithMessage($message, $image) {
     // Make a stop codon of 00000000
     for($i=strlen($binaryGiven);$i<strlen($binaryGiven) + 8;$i++){
     	if($i == 0) {
-        	$x = 0;
-        } else {
-        	$x = $i % $width;
-        }
+           $x = 0;
+       } else {
+           $x = $i % $width;
+       }
 
-        if($i == 0) {
-        	$y = 0;
-        } else {
-        	$y = $i / $width;
-        }
+       if($i == 0) {
+           $y = 0;
+       } else {
+           $y = $i / $width;
+       }
 
 
-    	$colors = imagecolorat($imageToModify, $x, $y);
-        $r = ($colors >> 16) & 0xFF;
-        $g = ($colors >> 8) & 0xFF;
-        $b = $colors & 0xFF;
-		$newR = $r;
-        if($r % 2 != 0) {
-        	if($r >= 255) {
-        		$newR = $r-1;
-        	} else {
-        		$newR = $r+1;
-        	}
-        } 
+       $colors = imagecolorat($imageToModify, $x, $y);
+       $r = ($colors >> 16) & 0xFF;
+       $g = ($colors >> 8) & 0xFF;
+       $b = $colors & 0xFF;
+       $newR = $r;
+       if($r % 2 != 0) {
+           if($r >= 255) {
+              $newR = $r-1;
+          } else {
+              $newR = $r+1;
+          }
+      } 
 
-        $newColor = imagecolorallocate($imageToModify, $newR, $g, $b);
-        imagesetpixel($imageToModify, $x, $y, $newColor);
-    }
+      $newColor = imagecolorallocate($imageToModify, $newR, $g, $b);
+      imagesetpixel($imageToModify, $x, $y, $newColor);
+  }
 
     //print("message hidden ".binaryToMessage($binaryGiven));
-    return $imageToModify;
+  return $imageToModify;
 
 }
 
@@ -121,7 +149,7 @@ function binaryToMessage($binary) {
     return $messageDecoded;
 }
 
-// echo messageToBinary("test")."\n";
+
 $im = encodeImageWithMessage($givenMessage, $target_img);
 // Set the content type header - in this case image/jpeg
 header('Content-Disposition: Attachment;filename=image.png'); 
