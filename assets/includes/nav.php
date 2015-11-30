97e21e0803adfb3c81c25896502b0fd22fe49843
dylanmodesitt <dylanmodesitt@gmail.com>
2015-11-29 16:55:51 -0800

    Standardized navigation

--- assets/includes/nav.php
+++ assets/includes/nav.php
@@ -10,28 +10,13 @@
       </button>
       <a class="navbar-brand" id="navbar-brand" href="./index.php">PICRYPTION</a>
     </div>
-
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="./index.php">Home<span class="sr-only">(current)</span></a></li>
-        <li><a href="./info.php">Info</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Encryption <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="./index.php">Message in image</a></li>
-            <li><a href="./image_obscure.php">Obscure image</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">More Coming Soon!</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" method="post" action="./search.php">
-        <div class="form-group">
-          <input type="text" class="form-control" name="search" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form> 
+        <li><a href="./info.php">Info<span class="sr-only">(current)</span></a></li>
+        <li><a href="./index.php">Encode</a></li>
+        <li><a href="./image_obscure.php">Obscure</a></li>
+        </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Legal <span class="caret"></span></a>
