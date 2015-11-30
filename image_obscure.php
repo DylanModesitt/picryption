703eddcf2fa5adb6c832abfb9f9995c4c4e11bcd
dylanmodesitt <dylanmodesitt@gmail.com>
2015-11-29 18:19:57 -0800

    Fixed typography issue

--- image_obscure.php
+++ image_obscure.php
@@ -8,7 +8,7 @@
 				<h1 class="red">Picryption<small> obscure image</small></h1>
 			</div>
 			<div>
-				<p class="big"> Picryption's obscure feature allows you to hide your image in plain sight. Upload your image, and Picryption will return a blank image, with the RGB values of each pixel changed according to an algorithm. You can then upload that image to Picryption and retrieve the original image.</p>
+				<p> Picryption's obscure feature allows you to hide your image in plain sight. Upload your image, and Picryption will return a blank image, with the RGB values of each pixel changed according to an algorithm. You can then upload that image to Picryption and retrieve the original image.</p>
 			</div>
 			<br><br>
 		</div>
@@ -20,7 +20,7 @@
 			<div class ="center-block text-left uploadContent">
 				<FORM METHOD='post' enctype="multipart/form-data"  class="form-style-1" ACTION='./assets/php/obscure.php'>
 					<div class= "text-left center-block col-sm-12">
-						<p class="big">Please upload the image that you would like to obscure.</p>
+						<p>Please upload the image that you would like to obscure.</p>
 						<input id="uploadFileOne" placeholder="Choose Image" disabled="disabled" />
 						<div class="fileUpload btn btn-danger">
 							<span>Upload Image</span>
@@ -42,7 +42,7 @@
 	</div>
 	<FORM METHOD='post' enctype="multipart/form-data"  ACTION='/assets/php/removeObscurity.php'>
 		<div class= "text-left center-block col-sm-12">
-			<p class="big">Upload an image that has been obscured by Picryption. The image will be downloaded.</p>
+			<p>Upload an image that has been obscured by Picryption. The image will be downloaded.</p>
 			<input id="uploadFileTwo" placeholder="Choose Image" disabled="disabled" />
 			<div class="fileUpload btn btn-danger">
 				<span>Upload Image</span>
